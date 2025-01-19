967c87aa05c1973f7e636db87756a5a2b6e17b89
w33zl <7383510+w33zl@users.noreply.github.com>
2025-01-19 11:27:48 +0100

    Initial commit

---
+++ FS25_BuyUsedEquipment/BuyUsedEquipment.lua
@@ -0,0 +1,31 @@
+--[[
+SHORT DESCRIPTION OF WHAT YOUR MOD DOES GOES HERE
+
+Author:     YOUR NAME/NICKNAME
+Version:    1.0.0
+Modified:   YYYY-MM-DD
+
+Changelog:
+
+]]
+
+--TODO: Uncomment the following lines and change the name of global variable 'YourModNameHere' to whatever you like your mod to be named
+--TODO: Check https://github.com/w33zl/FS22_WeezlsModLib for details about the "Mod" class
+
+-- YourModNameHere = Mod:init()
+
+-- Log:info("This is a test") -- This will output one line in the log with a prefix of your mod's name (requires the LogHelper.lua to be referenced in modDesc.xml)
+-- Log:debug("This only works in 'debug' mode") -- This will output one line in the log only if you have the DebugHelper.lua in your lib folder
+
+-- -- Event that is executed when your mod is loading (after the map has been loaded and before the game starts)
+-- function YourModNameHere:loadMap(filename)
+-- end
+
+-- -- Event that is continuously, USE WITH CAUTION! Any demanding code here (even just a simple "print()" command) will cause poor performance, stuttering and FPS drops
+-- function YourModNameHere:update(dt)
+-- end
+
+-- -- Event that is executed when the player chooses to start the mission (after the map has been loaded and before the game starts)
+-- function YourModNameHere:startMission()
+-- end
+
